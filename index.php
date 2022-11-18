1cbb585302335f7ce2dc739b86658de46c756e79
Kivi Amarakoon <89955961+kivi98@users.noreply.github.com>
2022-11-18 12:27:42 +0530

    logo added

--- index.php
+++ index.php
@@ -18,13 +18,26 @@
         <img class="profile_pic" src="images\doctor.png" alt="profile_pic">
     </nav> <!-- top navigation bar -- end -->
         <div class="main">
-        <div class="left">
-            <!-- <div><img class="login-imaage" src="images\mother.png" alt=""></div> -->
-        </div>
+        <div class="left"></div>
         <div class="right">
-            
+            <div class="login-container">
+                <img src="images/Project Logo - landscape-01 2.png" alt="login-logo">
+            </div>
         </div>
 
     </div>
-</body>
+</body><!-- <form class="login-form" action="#">
+                <fieldset>
+                <legend class="roll-frame" >Your roll:</legend>
+                    <input type="text" placeholder="Select">
+                </fieldset>
+                <fieldset>
+                    <legend>User name</legend>
+                    <input type="email" name="username" id="user-name" placeholder="Enter your user name">
+                </fieldset>
+                <fieldset>
+                    <legend>Password</legend>
+                    <input type="password" name="password" id="password" placeholder="Enter your password">
+                </fieldset>
+                </form> -->
 </html>
